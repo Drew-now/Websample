75e5c6085e9a41fc5ace1208df3741da61c1d49f
Drew-now <Drewkuehn@gmail.com>
2022-01-12 21:11:50 -0500

    Html Page

--- Mr_nibbles/scraper/urls.py
+++ Mr_nibbles/scraper/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.urls.conf import  path 
 from .views import main
+
 
 
 urlpatterns = [
--- Mr_nibbles/scraper/views.py
+++ Mr_nibbles/scraper/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 def main(request):
-    return HttpResponse("Hello Friend!!!") 
+    return render(request, "home.html")
+
+
+
